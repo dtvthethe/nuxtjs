51805fff5e58eefc6855f3087cfcf7bd43c81f42
Đàm Thế <dam.trieu.van.the@sun-asterisk.com>
2022-03-02 11:02:13 +0700

    Layout

---
+++ layouts/mobile.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <header class="mobile-header">
+			<h1>Mobile layout</h1>
+		</header>
+		<hr>
+		<Nuxt />
+		<hr>
+    <footer class="mobile-footer">
+			<h1>Mobile layout footer</h1>
+		</footer>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+	.mobile-header, .mobile-footer {
+		color: blueviolet;
+		text-align: center;
+	}
+</style>
---
+++ layouts/red.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <header>
+			<h1>Red layout</h1>
+		</header>
+		<hr>
+		<Nuxt />
+		<hr>
+    <footer>
+			<h1>Red layout footer</h1>
+		</footer>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
